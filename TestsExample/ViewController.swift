a86c6c07dc67dba5a7a72e9c0c4feb5049df7241
Hugo Rosado Oliden <hugo_andresro@hotmail.com>
2023-11-16 22:56:00 -0500

    First commit

---
+++ TestsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestsExample
+//
+//  Created by Hugo A. Rosado on 16/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestsExampleUITests/TestsExampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TestsExampleUITestsLaunchTests.swift
+//  TestsExampleUITests
+//
+//  Created by Hugo A. Rosado on 16/11/23.
+//
+
+import XCTest
+
+final class TestsExampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
