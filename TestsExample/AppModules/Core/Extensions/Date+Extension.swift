52d41c4a7cfb2970848de39b513300964ed77c29
Hugo Rosado Oliden <hugo_andresro@hotmail.com>
2023-11-18 21:42:52 -0500

    [a-1] Some extensions added.

--- TestsExample/AppModules/Core/Extensions/Date+Extension.swift
+++ TestsExample/AppModules/Core/Extensions/Date+Extension.swift
@@ -1,24 +1,20 @@
 //
-//  String+Extension.swift
+//  Date+Extension.swift
 //  TestsExample
 //
-//  Created by Hugo A. Rosado on 16/11/23.
+//  Created by Hugo A. Rosado on 18/11/23.
 //
 
 import Foundation
 
-extension String {
-    var firstCapitalized: Self {
-        prefix(1).capitalized + dropFirst()
-    }
-
+extension Date {
     func format(
         _ dateFormat: DateConstants,
         with timeZone: TimeZone? = nil
-    ) -> Date? {
+    ) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.timeZone = timeZone ?? .current
         dateFormatter.dateFormat = dateFormat.rawValue
-        return dateFormatter.date(from: self)
+        return dateFormatter.string(from: self)
     }
 }
---
+++ TestsExample/AppModules/Core/Presentation/Entity/DateConstants.swift
@@ -0,0 +1,16 @@
+//
+//  DateConstants.swift
+//  TestsExample
+//
+//  Created by Hugo A. Rosado on 18/11/23.
+//
+
+import Foundation
+
+enum DateConstants: String {
+    case basic = "yyyy-MM-dd HH:mm:ss"
+    case dateWithDash = "dd-MM-yyyy"
+    case hoursMinutes = "HH:mm"
+    case server = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+    case time = "HH:mm:ss"
+}
